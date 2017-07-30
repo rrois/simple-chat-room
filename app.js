823d5bbdf33c42adf4c1b7476cc3366000d3b145
Unknown <rois_r@hotmail.com>
2017-07-30 13:48:26 +0200

    Chat connection with sockets

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io  = require('socket.io')(server);
+
+io.on('connection', function(client){
+    console.log('Client connected...');
+
+    // Emite el evento 'newConnection' en el cliente(navegador)
+    client.emit('newConnection', {welcomeMessage: 'Te has unido al chat.'});
+});
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8080);
+server.listen(8080);
