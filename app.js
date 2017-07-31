e54b60654f437367694d091b5826243ea385c7a1
Unknown <rois_r@hotmail.com>
2017-07-31 14:36:49 +0200

    Chat messages between connected users

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io  = require('socket.io')(server);
+var io = require('socket.io')(server);
 
-io.on('connection', function(client){
+io.on('connection', function (client) {
     console.log('Client connected...');
 
     // Emite el evento 'newConnection' en el cliente(navegador)
-    client.emit('newConnection', {welcomeMessage: 'Te has unido al chat.'});
+    client.emit('newConnection', { welcomeMessage: 'Te has unido al chat.' });
+
+    // Escucha el evento 'message'
+    client.on('message', function (data) {
+        // Broadcast del mensaje
+        client.broadcast.emit('message', data);
+
+        // Se envía el mismo mensaje a nuestro cliente
+        client.emit('message', data);
+
+        console.log(data);
+    });
 });
 
 app.get('/', function (request, response) {
